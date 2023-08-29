b9adf786c901cce10d78fe241f83a01160eacb78
Grace kungu <grayckungu2@gmail.com>
2023-08-29 20:53:31 +0300

    update changes

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,30 @@
-#!/usr/bin/env python3
-
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
-
 class Dog:
-    pass
+    def __init__(self, name, breed="Labrado "):
+        self._name = name
+        self._breed = breed
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        approved_breeds = ["Mutt", "Labrador", "Golden Retriever", "German Shepherd"]
+        if value in approved_breeds:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    def sit(self):
+        print("The dog is sitting.")
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python3
+class Person:
+    def __init__(self, name):
+        self._name = name
 
-APPROVED_JOBS = [
-    "Admin",
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be a string under 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        approved_jobs = [
+        "Admin",
     "Customer Service",
     "Human Resources",
     "ITC",
@@ -13,7 +33,14 @@ APPROVED_JOBS = [
     "Research & Development",
     "Marketing",
     "Purchasing"
-]
+        ]
+        if value in approved_jobs:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
 
-class Person:
-    pass
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
